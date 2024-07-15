3d5aa57a9a9bd06d636eb7c293ba7fbf006ca28d
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-07-15 12:20:00 +0545

    set up lint-staged husky and prettier

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
