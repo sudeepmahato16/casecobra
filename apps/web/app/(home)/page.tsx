80dd5c78c5f4141a5daa75825901031d151456cf
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-07-15 16:53:04 +0545

    add hero section

---
+++ apps/web/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./_components/Hero";
+
+const Home = () => {
+  return (
+    <div className="bg-slate-50 grainy-light">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
---
+++ apps/web/components/Phone.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@casecobra/ui";
+import Image from "next/image";
+import { HTMLAttributes } from "react";
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <Image
+        width="0"
+        height="0"
+        sizes="100vw"
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        className="pointer-events-none z-50 select-none w-auto h-auto"
+        alt="phone image"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <Image
+          className="object-cover min-w-full min-h-full"
+          width="0"
+          height="0"
+          sizes="100vw"
+          src={imgSrc}
+          alt="overlaying phone image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
