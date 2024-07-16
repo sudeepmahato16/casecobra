2e1e3fb4cf6ba759137e587bee357c201b5b829c
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-07-16 10:53:31 +0545

    add proposition section

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./_components/Hero";
+import Proposition from "./_components/Proposition";
 
 const Home = () => {
   return (
     <div className="bg-slate-50 grainy-light">
       <Hero />
+      <Proposition />
     </div>
   );
 };
