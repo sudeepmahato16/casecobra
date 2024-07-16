654e452ef30ad29f3bae6032e59adf42ec77593b
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-07-16 13:44:44 +0545

    add footer

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Recursive } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">{children}</div>
         </main>
+        <Footer />
       </body>
     </html>
   );
