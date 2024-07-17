c18ec79bb4eb5ae3da08a2822f4156bf4d4ecd7b
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-07-17 12:48:11 +0545

    feat: add user sign up functionality

--- apps/api/controller/authController.ts
+++ apps/api/controller/authController.ts
@@ -1,5 +1,35 @@
+import * as bcrypt from "bcrypt";
+import { db } from "@casecobra/db";
 import { catchAsync } from "@/utils/catchAsync";
+import AppError from "@/utils/appError";
 
 export const signUp = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
+
+  const isEmailInUse = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (isEmailInUse) return next(new AppError("Email is in use", 400));
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  try {
+    await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Please verified your email address",
+    });
+  } catch (error) {
+    return next(new AppError("Failed to create account", 500));
+  }
 });
