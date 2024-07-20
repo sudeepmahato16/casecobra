deff18cbe060f02fe187e74c4e7c3e1243a046ed
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-07-20 14:21:11 +0545

    add steps in configure page

--- apps/web/app/(routes)/configure/layout.tsx
+++ apps/web/app/(routes)/configure/layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Steps from "@/features/configure/Steps";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
